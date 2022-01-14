2321ae513b3807730530d0222609e0c2db04cd73
Slawomir Kulig <slawomir.kulig@comarch.com>
2022-01-14 18:16:51 +0100

    security

--- src/main/java/com/crypto/binance/configuration/ApplicationConfiguration.java
+++ src/main/java/com/crypto/binance/configuration/ApplicationConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.crypto.binance.configuration.openapi.OpenApiConfiguration;
+import com.crypto.binance.configuration.security.SecurityConfiguration;
 
 @Configuration
-@Import(OpenApiConfiguration.class)
+@Import({
+            OpenApiConfiguration.class,
+            SecurityConfiguration.class
+        })
 public class ApplicationConfiguration {
 
 }
