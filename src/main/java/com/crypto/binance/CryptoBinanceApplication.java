891dfeec15585a1bc47cbbb521217d40ecee4727
Slawomir Kulig <slawomir.kulig@comarch.com>
2022-01-14 15:19:12 +0100

    init

---
+++ src/main/java/com/crypto/binance/CryptoBinanceApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.binance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoBinanceApplication.class, args);
+    }
+
+}
